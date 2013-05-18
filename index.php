e6b2e79379a74ae6cc87f9777d713f6696064a3d
Martin M Reed <martin.reed@hallowtech.com>
2013-05-18 13:55:22 -0600

    Adding "fork me" ribbon

--- index.php
+++ index.php
@@ -13,6 +13,11 @@ require("include/common_sql.php");
     </style>
   </head>
   <body>
+    <a href="https://github.com/hardisonbrewing/signingserver-com">
+      <img style="position:absolute; top:0; right:0; border:0;"
+           src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
+           alt="Fork me on GitHub">
+    </a>
     <?php show_log('RRT', 'RIM Runtime') ?><br />
     <hr>
     <?php show_log('RBB', 'RIM Blackberry Apps') ?><br />
