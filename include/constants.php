78a42d0f8d9aa3c88c91c51d53de94d76e3f8bf1
Martin M Reed <martin.reed@hallowtech.com>
2013-05-30 13:44:32 -0600

    Update constants.php

--- include/constants.php
+++ include/constants.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 // accepted signatures
@@ -23,5 +24,32 @@ define("OAUTH_SECRET", "");
 
 // number of failures before it goes to twitter
 define("TWEETER_THRESHOLD", "3");
+=======
+<?php 
+  
+  // accepted signatures
+  define("VALID_SIG", "rrt,rbb,rcr,rcc,pbk");
+  define("VALID_CHART_SIG", VALID_SIG.",all");
+  define("VALID_CHART_SIG_COMP", "rrt,rbb,rcr,rcc,all");
+  
+  // apikey for submit.php
+  define("SUBMISSION_KEY", "");
+  
+  // database connection
+  define("DB_HOST", "");
+  define("DB_USER", "");
+  define("DB_PASSWD", "");
+  define("DB_NAME", "");
+  define("DB_TABLE", "");
+  
+  // twitter connection
+  define("CONSUMER_KEY", "");
+  define("CONSUMER_SECRET", "");
+  define("OAUTH_TOKEN", "");
+  define("OAUTH_SECRET", "");
+  
+  // number of failures before it goes to twitter
+  define("TWEETER_THRESHOLD", "3");
+>>>>>>> 7c963a2... Update constants.php
 
 ?>
